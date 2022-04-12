c0955e344bf227740ff888522b7a93d67383e011
ArtemSpivak <spivak25@yandex.ru>
2022-04-12 20:32:51 +0300

    Задача решена

--- Лекция 2/Задача 3 МассивМетод/Program.cs
+++ Лекция 2/Задача 3 МассивМетод/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int[] array ={1,2,3,4,18,5,6,7,18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
