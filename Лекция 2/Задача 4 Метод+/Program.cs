5cbb7b1222759f2f9c7163f95dd7f5885f807a55
ArtemSpivak <spivak25@yandex.ru>
2022-04-12 20:52:16 +0300

    Задача решена, с комментариями

---
+++ Лекция 2/Задача 4 Метод+/Program.cs
@@ -0,0 +1,29 @@
+﻿//Метод заполнения массива случайными числами от 1 до 10
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index ++;
+    }
+}
+
+//Метод вывода на печать массива
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+//Определяем массив из 10 элементов
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
