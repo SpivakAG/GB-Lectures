fcd427516c0029a64fcfce857f09c0bb70501cdd
ArtemSpivak <spivak25@yandex.ru>
2022-04-14 21:26:43 +0300

    решение задачи с функциями

--- Семинар 2/Задача 2/Program.cs
+++ Семинар 2/Задача 2/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int Third (int number1)
+{
+    int first = number % 10;
+    return first;
+}
+
+int First (int number1)
+{
+    int first = number / 100;
+    return first;
+}
+
+int Sum (int n1, int n2)
+{
+    int dozens= n1*10;
+    int result = dozens + n2;
+    return result;
+}
+
+int first = First (number);
+int third = Third (number);
+
+int result = Sum(first, third);
+Console.WriteLine(result);
