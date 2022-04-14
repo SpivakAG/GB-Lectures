d7aaecb8217432a2af185cdb54b20c058d9180ec
ArtemSpivak <spivak25@yandex.ru>
2022-04-14 20:35:59 +0300

    задача решена

---
+++ Семинар 2/Задача 1/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100);
+int first = number / 10;
+int second = number % 10;
+
+if (first>second)
+{
+    Console.WriteLine(first);
+}
+else
+{
+    Console.Writeline(second);
+}
