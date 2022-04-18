2b65493aa3a1e167982b089e8adac7a3458e0205
ArtemSpivak <spivak25@yandex.ru>
2022-04-18 21:49:14 +0300

    задача решена

--- Семинар 3/Задача 4/Program.cs
+++ Семинар 3/Задача 4/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Введите число:");
+double number = Convert.ToDouble(Console.ReadLine());
+double index = 1;
+
+while (index <= number)
+{
+    double result = Math.Pow(index, 2);
+    index +=1;
+    Console.Write($"{result} ");
+}
