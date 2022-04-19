e05864a1f5758de55fca673d6b8326a05fe886d6
ArtemSpivak <spivak25@yandex.ru>
2022-04-19 20:22:24 +0300

    Задача решена

--- Лекция  3/Задача 2/Program.cs
+++ Лекция  3/Задача 2/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿//Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы “к” заменить большими “К”, а большие “С” заменить маленькими “с”.
+
+string text = "- Я думаю, - сказал князь, улыбаясь, -что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//            012345
+//s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; //инициализация пустой строки
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}"; 
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '-'); //при работе с char используются ' ' 
+newText = Replace(newText, 'к', 'К');
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
