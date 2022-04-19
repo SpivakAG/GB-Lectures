fa53ae9fbf4f098870ce0ebea6aa074188adae30
ArtemSpivak <spivak25@yandex.ru>
2022-04-19 20:46:28 +0300

    решение задачи

--- Лекция  3/Доп задача/Program.cs
+++ Лекция  3/Доп задача/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Упорядочить данные в массиве. Алгоритм сортировки методом выбора
 // Дано 1, 5, 4, 3, 2, 6 ,7 ,1 ,1
-// 1 Найти позицию минимального элемента в неотсортированной части массива
+// 1 Найти позицию максимального элемента в неотсортированной части массива
 // 2 Произвести обмен этого значения со значением первой неотсортированной позиции
 // 3 Повторять пока есть не отсортировнные элементы
 
@@ -21,18 +21,17 @@ void SelectionSort(int[] array)
 {
     for (int i = 0; i < array.Length -1; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
 
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] > array[maxPosition]) maxPosition = j;
         }
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     }
 }
-
 
 PrintArray(arr);
 SelectionSort(arr);
