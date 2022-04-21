5a6320954eaf43b171a8e5f6309660de7ba1ec2f
ArtemSpivak <spivak25@yandex.ru>
2022-04-21 20:43:56 +0300

    задача решена с исправлением

--- Семинар 4/Задача 1/Program.cs
+++ Семинар 4/Задача 1/Program.cs
@@ -1,9 +1,11 @@
-﻿Console.WriteLine("Введите число");
+﻿//Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int sum = 0;
 
-while (number > 1)
+while (number > 0)
 {
     number = number / 10;
     sum ++;
