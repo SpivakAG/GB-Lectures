22976754dc57aed170bd3b8c72b8409e5c5ed656
ArtemSpivak <spivak25@yandex.ru>
2022-04-21 20:29:08 +0300

    задача решена

--- Семинар 4/Задача 1/Program.cs
+++ Семинар 4/Задача 1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 1)
+{
+    number = number / 10;
+    sum ++;
+}
+Console.WriteLine("Количество цифр в числе равно " +sum );
