ffefefa6394af6bb3e0b1b0172b5f20b186b49c5
ArtemSpivak <spivak25@yandex.ru>
2022-04-21 21:05:06 +0300

    задача решена

--- Семинар 4/Задача 2/Program.cs
+++ Семинар 4/Задача 2/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = 1;
+
+// Вариант решения через while
+
+/*int index = 1;
+while (index <= number)
+{
+    factorial = factorial*index;
+    index ++;
+} */
+
+// Вариант решения через for
+
+for (int i = 1; i <= number; i++)
+{
+    factorial = factorial*i;
+}
+
+
+Console.WriteLine("Факториал числа " +number +" равен " +factorial);
